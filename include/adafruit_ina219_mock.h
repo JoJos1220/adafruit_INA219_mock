28d23ac57ee7058e2f37a61b3f431da6f4a084dc
JoJos1220 <97045955+JoJos1220@users.noreply.github.com>
2023-05-24 16:57:44 +0200

    Further Project based changes due to tests

--- include/adafruit_ina219_mock.h
+++ include/adafruit_ina219_mock.h
@@ -21,20 +21,39 @@
 */
 #include <stdint.h>
 
+// Define default I2C address
+#define INA219_ADDRESS (0x40)
+
 /*-----------------------------------------------------------------------------
  * Adafruit_INA219 MOCK Class definition
  * ----------------------------------------------------------------------------
 */
 class Adafruit_INA219 {
 public:
+  Adafruit_INA219(uint8_t addr = INA219_ADDRESS);
+  ~Adafruit_INA219();
   bool begin(uint8_t addr = 0x40);
   void setCalibration_32V_2A(void);
+  void setCalibration_32V_1A(void);
+  void setCalibration_16V_400mA(void);
+
+  float getBusVoltage_V(void);
+  float getShuntVoltage_mV(void);
+  float getCurrent_mA(void);
+  float getPower_mW(void);
+  void powerSave(bool on);
+
+  bool success(void);
+
+private:
+  uint8_t i2caddr;
+  uint32_t ina219_currentDivider_mA;
+  float ina219_powerMultiplier_mW;
+
   int16_t getBusVoltage_raw(void);
   int16_t getShuntVoltage_raw(void);
   int16_t getCurrent_raw(void);
   int16_t getPower_raw(void);
-private:
-  uint8_t i2caddr;
 };
 
 /*-----------------------------------------------------------------------------
@@ -46,8 +65,10 @@ void setFakeBusVoltage(int16_t value);
 void setFakeShuntVoltage(int16_t value);
 void setFakeCurrent(int16_t value);
 void setFakePower(int16_t value);
+void setFakeSuccess(bool value);
 
 bool getINA219Begin(void);
+bool getPowerSave(void);
 
 #endif // ADAFRUIT_INA219_MOCK_H
 
--- src/adafruit_ina219_mock.cpp
+++ src/adafruit_ina219_mock.cpp
@@ -4,12 +4,30 @@
  * Declaration/Definition of static Libary internal Variables
  * ----------------------------------------------------------------------------
 */
-static int16_t fake_int16_BusVoltage = 0; // Define a static variable to store the fake value
+// Definition of static variables to store fake values
+static int16_t fake_int16_BusVoltage = 0; 
 static int16_t fake_int16_Current = 0;
 static int16_t fake_int16_ShuntVoltage = 0;
 static int16_t fake_int16_Power = 0;
+static bool fake_bool_success = false;
 static bool fake_bool_begin = false;
 static bool return_bool_begin = false;
+static bool return_bool_powerSave = false;
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::Konstructor
+ * ----------------------------------------------------------------------------
+*/
+Adafruit_INA219::Adafruit_INA219(uint8_t addr){
+  ina219_currentDivider_mA = 0;
+  ina219_powerMultiplier_mW = 0.0f;
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::Destructor
+ * ----------------------------------------------------------------------------
+*/
+Adafruit_INA219::~Adafruit_INA219(){}
 
 /*-----------------------------------------------------------------------------
  * Adafruit_INA219::begin Method
@@ -17,19 +35,39 @@ static bool return_bool_begin = false;
 */
 bool Adafruit_INA219::begin(uint8_t addr) {
   return_bool_begin = fake_bool_begin;    // Set Global Return Value for TestUnit
+  setCalibration_32V_2A();
   return fake_bool_begin;
 }
 
 /*-----------------------------------------------------------------------------
- * Adafruit_INA219::SetCalibration Method
+ * Adafruit_INA219::SetCalibration Method 32V/2A
  * ----------------------------------------------------------------------------
 */
 void Adafruit_INA219::setCalibration_32V_2A(void) {
-  // Return Nothing
+  ina219_currentDivider_mA = 10; // Current LSB = 100uA per bit (1000/100 = 10)
+  ina219_powerMultiplier_mW = 2; // Power LSB = 1mW per bit (2/1)
 }
 
 /*-----------------------------------------------------------------------------
- * Adafruit_INA219::getBusVoltage Method
+ * Adafruit_INA219::SetCalibration Method 32V/1A
+ * ----------------------------------------------------------------------------
+*/
+void Adafruit_INA219::setCalibration_32V_1A(void) {
+  ina219_currentDivider_mA = 25;    // Current LSB = 40uA per bit (1000/40 = 25)
+  ina219_powerMultiplier_mW = 0.8f; // Power LSB = 800uW per bit 
+};
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::SetCalibration Method 16V/400mA
+ * ----------------------------------------------------------------------------
+*/
+void Adafruit_INA219::setCalibration_16V_400mA(void) {
+  ina219_currentDivider_mA = 20;    // Current LSB = 50uA per bit (1000/50 = 20)
+  ina219_powerMultiplier_mW = 1.0f; // Power LSB = 1mW per bit
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::getBusVoltage_raw Method
  * ----------------------------------------------------------------------------
 */
 int16_t Adafruit_INA219::getBusVoltage_raw(void) {
@@ -37,7 +75,7 @@ int16_t Adafruit_INA219::getBusVoltage_raw(void) {
 }
 
 /*-----------------------------------------------------------------------------
- * Adafruit_INA219::getShuntVoltage Method
+ * Adafruit_INA219::getShuntVoltage_raw Method
  * ----------------------------------------------------------------------------
 */
 int16_t Adafruit_INA219::getShuntVoltage_raw(void) {
@@ -45,7 +83,7 @@ int16_t Adafruit_INA219::getShuntVoltage_raw(void) {
 }
 
 /*-----------------------------------------------------------------------------
- * Adafruit_INA219::getCurrent Method
+ * Adafruit_INA219::getCurrent_raw Method
  * ----------------------------------------------------------------------------
 */
 int16_t Adafruit_INA219::getCurrent_raw(void) {
@@ -53,11 +91,59 @@ int16_t Adafruit_INA219::getCurrent_raw(void) {
 }
 
 /*-----------------------------------------------------------------------------
- * Adafruit_INA219::getPower Method
+ * Adafruit_INA219::getPower_raw Method
  * ----------------------------------------------------------------------------
 */
 int16_t Adafruit_INA219::getPower_raw(void) {
   return fake_int16_Power;
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::getShuntVoltage_mV Method
+ * ----------------------------------------------------------------------------
+*/
+float Adafruit_INA219::getShuntVoltage_mV(void){
+  return Adafruit_INA219::getShuntVoltage_raw();
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::getBusVoltage_V Method
+ * ----------------------------------------------------------------------------
+*/
+float Adafruit_INA219::getBusVoltage_V(void){
+  return Adafruit_INA219::getBusVoltage_raw();
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::getCurrent_mA Method
+ * ----------------------------------------------------------------------------
+*/
+float Adafruit_INA219::getCurrent_mA(void) {
+  return (float)Adafruit_INA219::getCurrent_raw();  
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::getPower_mW Method
+ * ----------------------------------------------------------------------------
+*/
+float Adafruit_INA219::getPower_mW(void) {
+  return (float)Adafruit_INA219::getPower_raw();
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::powerSave Method
+ * ----------------------------------------------------------------------------
+*/
+void Adafruit_INA219::powerSave(bool on){
+  return_bool_powerSave = on;
+}
+
+/*-----------------------------------------------------------------------------
+ * Adafruit_INA219::success Method
+ * ----------------------------------------------------------------------------
+*/
+bool Adafruit_INA219::success(void){
+  return fake_bool_success;
 }
 
 /*-----------------------------------------------------------------------------
@@ -101,9 +187,25 @@ void setFakeBegin(bool value){
 }
 
 /*-----------------------------------------------------------------------------
- * getINA219 Method to Adafruit_INA219 class
+ * setFakeSuccess Method to Adafruit_INA219 class
+ * ----------------------------------------------------------------------------
+*/
+void setFakeSuccess(bool value){
+  fake_bool_success = value;
+}
+
+/*-----------------------------------------------------------------------------
+ * getINA219Begin Method to Adafruit_INA219 class
  * ----------------------------------------------------------------------------
 */
 bool getINA219Begin(void){
   return return_bool_begin;
+}
+
+/*-----------------------------------------------------------------------------
+ * getPowerSave Method to Adafruit_INA219 class
+ * ----------------------------------------------------------------------------
+*/
+bool getPowerSave(void){
+  return return_bool_powerSave;
 }
